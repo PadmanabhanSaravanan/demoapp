30bf29167aef8d8611ffa529487eafe990a88af5
PadmanabhanSaravanan <g.s.r.padmanabhan@gmail.com>
2025-05-10 11:28:49 +0530

    updated angular app

---
+++ task-app/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    allowedHosts: ["https://demoapp-v3m0.onrender.com/"],
+    host: "https://demoapp-v3m0.onrender.com/",
+  },
+});
