81cf8a0f15baaa637506c82d50a76bb3d20180f7
PadmanabhanSaravanan <g.s.r.padmanabhan@gmail.com>
2025-05-10 11:41:00 +0530

    Downgraded to 17

--- task-app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: {
-    allowedHosts: ["https://demoapp-v3m0.onrender.com/"],
-    host: "https://demoapp-v3m0.onrender.com/",
-  },
-});
